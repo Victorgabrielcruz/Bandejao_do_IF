a374136df10f3cd9ff4b7d695b8c7a3efcf62d0f
victor <victorgabrielcruzpereira21@gmail.com>
2026-03-17 19:44:43 -0300

    init

---
+++ backend/src/bandejao/src/test/java/com/bandejao/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandejao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
